50d60e6f9b5f0c5e7d02594ed1a0c6d926694548
Burton Pinckney <mbpinckney@gmail.com>
2019-09-10 22:26:09 -0400

    Updates

---
+++ precache-manifest.c614da4650336c57cf811bb4aa8247b6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f77ec1b2504f3ef04dfd713aa63527",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "a16020c0f9c857fd6ff9",
+    "url": "/ClickyGame/static/css/main.0f8fdfce.chunk.css"
+  },
+  {
+    "revision": "2443b78906742104aea5",
+    "url": "/ClickyGame/static/js/2.9b4104ab.chunk.js"
+  },
+  {
+    "revision": "a16020c0f9c857fd6ff9",
+    "url": "/ClickyGame/static/js/main.559b3b8a.chunk.js"
+  },
+  {
+    "revision": "76577335f12752be9d89",
+    "url": "/ClickyGame/static/js/runtime~main.2d8f2f60.js"
+  },
+  {
+    "revision": "81174006dedf38df36a504b3ca87432c",
+    "url": "/ClickyGame/static/media/clemsonmascot.81174006.jpg"
+  },
+  {
+    "revision": "2c40f160cfa07856fd1838d788abd986",
+    "url": "/ClickyGame/static/media/ecumascot.2c40f160.jpg"
+  },
+  {
+    "revision": "c6284dc011a906d4f3ede52cc7ab388c",
+    "url": "/ClickyGame/static/media/floridamascot.c6284dc0.jpg"
+  },
+  {
+    "revision": "46cd97e0173526a86ef81d925acb0e98",
+    "url": "/ClickyGame/static/media/georgiamascot.46cd97e0.jpg"
+  },
+  {
+    "revision": "0fabe27ea9711853cd4a26966a76c581",
+    "url": "/ClickyGame/static/media/lsumascot.0fabe27e.jpg"
+  },
+  {
+    "revision": "9af4b165398a7d74830f1f03ee294876",
+    "url": "/ClickyGame/static/media/ncstatemascot.9af4b165.jpg"
+  },
+  {
+    "revision": "4f4af9e650e2d488c62ddbb9b8cad93f",
+    "url": "/ClickyGame/static/media/scmascot.4f4af9e6.jpg"
+  },
+  {
+    "revision": "c0defacbe6358141796ab1bcbea81daf",
+    "url": "/ClickyGame/static/media/tennesseemascot.c0defacb.jpg"
+  },
+  {
+    "revision": "d0fe7d4434235a73552dc309dfad56a1",
+    "url": "/ClickyGame/static/media/wfmascot.d0fe7d44.jpg"
+  }
+]);
